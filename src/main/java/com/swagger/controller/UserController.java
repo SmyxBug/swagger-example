97a9e6c925c3a54a2ca81e42f3b44c5d01a6f4ee
SmyxCoding <itchangelives@126.com>
2020-10-03 23:20:48 +0800

    Swagger Example full push

--- src/main/java/com/swagger/controller/UserController.java
+++ src/main/java/com/swagger/controller/UserController.java
@@ -2,6 +2,8 @@ package com.swagger.controller;
 
 import com.swagger.models.UserEntity;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,17 @@ public class UserController {
     @GetMapping("/users/{id}")
     public UserEntity findById(@PathVariable Long id) {
         return new UserEntity(id.intValue(), "呵呵", 20, "男", "上海市浦东新区");
+    }
+
+    @ApiOperation("根据用户类型和手机号获取用户名")
+    @ApiImplicitParams({
+            // 其余属性并无他用需要自行Google或者查看相关api文档
+            @ApiImplicitParam(name = "type", value = "用户类型"),
+            @ApiImplicitParam(name = "cellphone", value = "手机号码")
+    })
+    @PostMapping("/getUserName")
+    public String getUserName(String type, String cellphone) {
+        return "smyx";
     }
 
 }
--- src/main/java/com/swagger/v3/Swagger3Config.java
+++ src/main/java/com/swagger/v3/Swagger3Config.java
@@ -3,21 +3,48 @@ package com.swagger.v3;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class Swagger3Config {
+
+    @Resource
+    Environment environment;
+
     @Bean
     public Docket createRestApi() {
+        // 获取全局的环境变量判别是在哪个环境
+        Profiles profiles = Profiles.of("prod");
+        boolean isAccept = environment.acceptsProfiles(profiles);
+        // 配置每个请求的全局属性 例如：header 参数里面放置token等
+        List<RequestParameter> params = new ArrayList<>();
+        RequestParameter token = new RequestParameterBuilder().name("token")
+                .description("全局token,请求header必带")
+                .in(ParameterType.HEADER)
+                .required(true)
+                .build();
+        params.add(token);
         return new Docket(DocumentationType.OAS_30)
+//                .ignoredParameterTypes(Integer.class, Long.class) // 忽略展示部分参数
+                .enable(!isAccept) // 配置是否可以浏览器访问接口信息 一般通过SpringBoot yml 文件去判定当前在扫描环境下
                 .apiInfo(apiInfo())
+                .globalRequestParameters(params)
                 .select()
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)) // 扫描策略：只扫描带有ApiOperation注解的方法 PS：扫描策略有很多自己去Google或者查看相关api文档
                 .build();
     }
 
@@ -29,4 +56,21 @@ public class Swagger3Config {
                 .version("1.0.0")
                 .build();
     }
+
+    /**
+     * swagger请求分组的写法
+     * 其实就是配置多个bean一个bean处理一个组别
+     */
+//    @Bean
+//    public Docket userDocket() {
+//        Profiles profiles = Profiles.of("prod");
+//        boolean isAccept = environment.acceptsProfiles(profiles);
+//        return new Docket(DocumentationType.OAS_30)
+//                .enable(!isAccept)
+//                .apiInfo(apiInfo())
+//                .groupName("用户请求模块")
+//                .select()
+//                .paths(PathSelectors.ant("/user"))
+//                .build();
+//    }
 }
